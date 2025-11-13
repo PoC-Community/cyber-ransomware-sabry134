1e1460504913f058ab3d180dcad1b3eedf2f026d
Aurelien <aurelien.duval@epitech.eu>
2025-11-13 19:06:06 +0100

    [ADD] final push for unit tests

--- tests/unit_tests_ransom.c
+++ tests/unit_tests_ransom.c
@@ -1,30 +1,25 @@
-/*
-** Author:  PocInnovation Workshop
-** Last Edited:
-** File Description: assertions test for ransom.c
-*/
-
-#include "ransom.h"
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include <criterion/criterion.h>
+#include <sodium.h>
+#include <stdio.h>
+#include <string.h>
+#include "../sources/ransom.h"
 
-Test(get_new_path_name, test_get_new_path_name)
-{
-    char *parentPath = "/home";
-    char *currentPath = "naadi/";
-    char test[MAX_FILEPATH];
+#define TEST_PASSWORD "testpassword"
+#define TEST_SALT_LEN 16
+#define TEST_FILE "tests/testfile.txt"
+#define ENCRYPTED_FILE "tests/testfile.txt.ransom"
+#define DECRYPTED_FILE "tests/testfile.txt.decrypted"
 
-    get_new_path_name(parentPath, test, currentPath);
-    cr_assert_str_eq(test, "/home/naadi/");
+unsigned char test_salt[TEST_SALT_LEN];
+
+void create_test_file() {
+    FILE *fp = fopen(TEST_FILE, "w");
+    fputs("This is a test string.\nAnother line.\n", fp);
+    fclose(fp);
 }
 
-Test(add_file_extension, test_add_file_extension)
-{
-    char *fileName = "file.odt";
-    char test[MAX_FILEPATH];
-
-    add_file_extension(fileName, test);
-    cr_assert_str_eq(test, "file.odt.ransom");
+void cleanup_test_files() {
+    remove(TEST_FILE);
+    remove(ENCRYPTED_FILE);
+    remove(DECRYPTED_FILE);
 }

